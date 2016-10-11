a222579c04ade4e2bed9e3b7a0b866741abbf396
laurent <laurent.cetinsoy@gmail.com>
2016-10-11 19:05:07 +0200

    Laplacian sharpening v1

--- TP4/TP4.m
+++ TP4/TP4.m
@@ -1,7 +1,13 @@
 
-image = double(imread("input/Monet_Les_Nymphéas.jpg"));
+image = double(imread("input/Monet_Les_Nymphéas.jpg")) / 255;
+figure; title("Monet Les Nymphéas");
+imshow(image);
 
-sharped = laplacianSharpening(image, 3);
+
+sharped = laplacianSharpening(image, 3, 0.4);
 
 figure; title("Laplacian Sharpening");
 imshow(sharped);
+
+
+pause
--- TP4/laplacianSharpening.m
+++ TP4/laplacianSharpening.m
@@ -1,17 +1,18 @@
-function image = laplacianSharpening(image, n)
+function image = laplacianSharpening(image, n, epsilon = 0.01)
 
-  laplacien_x = zeros(size(image));
+  laplacien_x = ones(size(image));
+
   laplacien_y = laplacien_x;
 
   for i = 1 : n
 
-    laplacien_x(1: end-1, :) = image(1: end-1, :) - image(2: end, :);
-    laplacien_x(1: end-1, :) = laplacien_x(1: end-1, :) - laplacien_x(2: end, :);
+    laplacien_x(1: end-1, :, : ) = image(1: end-1, :, :) - image(2: end, :, :);
+    laplacien_x(1: end-1, :, : ) = laplacien_x(1: end-1, :, :) - laplacien_x(2: end, :, :);
 
-    laplacien_y(:, 1: end-1) = image(:, 1: end-1) - image(2: end, :);
-    laplacien_y(:, 1: end-1) = laplacien_y(:, 1: end-1) - laplacien_y(:, 2: end);
+    laplacien_y(:, 1: end-1, :) = image(:, 1: end-1, :) - image(:, 2: end, :);
+    laplacien_y(:, 1: end-1, :) = laplacien_y(:, 1: end-1, :) - laplacien_y(:, 2: end, :);
 
-    image = image - (laplacien_x + laplacien_y);
+    image = image - epsilon * (laplacien_x + laplacien_y);
 
   end
 
