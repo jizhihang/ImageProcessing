5fddb6a1f92115b88e1d83ef7516b2c73c0c3785
Aris Tritas <a.tritas@gmail.com>
2016-10-23 06:11:54 +0200

    TP5 finalise

--- TP5/test_bilateral_filter.m
+++ TP5/test_bilateral_filter.m
@@ -8,12 +8,12 @@ A = double(imread('images/flowers.bmp'))/255;
 A = A+0.1*randn(size(A));
 % A(A<0) = 0; A(A>1) = 1;
 
-%imwrite(A, "noisy.png");
+imwrite(A, "output/flowers_noisy.png");
 
 % apply the filter
-B = bilateral_filter(A,3,1,1);
+%B = bilateral_filter(A,3,1,1);
 
-imwrite(B, "output/flowers_filtered.png");
+%imwrite(B, "output/flowers_filtered.png");
 
 % Visualize the histograms if we want.
 % figure;
